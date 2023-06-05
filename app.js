75e14ec66e84c4f8b5ccdad0cca4ac7f1d488ddb
정진철 <jincheol@jeongjincheol-ui-MacBookPro.local>
2023-06-05 14:45:35 +0900

    cors port 추가

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import { swaggerUi, specs } from "./src/swagger/swagger.js";
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // 접근 권한을 부여하는 도메인들의 배열
+    origin: ["http://localhost:5173", "http://localhost:5174" ], // 접근 권한을 부여하는 도메인들의 배열
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
 }));
